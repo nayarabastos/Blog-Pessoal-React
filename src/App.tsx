dae11047e51792de8175d01272c6a33cb8c63b53
Nayara <bastosnayara24@gmail.com>
2026-08-17 13:53:15 -0300

    💄 UI:  Estilização dos Componentes Home, Navbar e Footer Utilizando Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Home from "./pages/home/Home"
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
 
 function App() {
 
 
   return (
     <>
+      <Navbar/>
       <Home/>
+      <Footer/>
     </>
   )
 }
